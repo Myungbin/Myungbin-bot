443ca7dcdf84855d5203eea5a86f1ef2236ae180
myungbin <kki9621@naver.com>
2023-05-31 18:45:41 +0900

    Add Preprocessing 데이터 전처리 추가

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 from src.config import cfg
 
 
-def process_personal_talk_file(file_path):
+def preprocessed_personal_talk_file(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         data = file.read()
 
@@ -22,11 +22,49 @@ def process_personal_talk_file(file_path):
     return processed_data
 
 
-def save_processed_data(output_file_path):
+def save_preprocessed_data(output_file_path, process_data):
     with open(output_file_path, "w", encoding="utf-8") as file:
         for sender, message in process_data:
             file.write(f"[{sender}] {message}\n")
 
 
-process_data = process_personal_talk_file(cfg.RAW_FILE_PATH)
-print(process_data)
+def convert_preprocessed_data(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = file.read()
+
+    lines = data.split("\n")
+    formatted_data = []
+    current_sender = None
+    current_message = ""
+
+    for line in lines:
+        if line.startswith("[Q]"):
+            sender = "Q"
+            message = line[4:].strip()
+        elif line.startswith("[A]"):
+            sender = "A"
+            message = line[4:].strip()
+        else:
+            continue
+
+        if current_sender is None:
+            current_sender = sender
+
+        if current_sender == sender:
+            current_message += " " + message
+        else:
+            formatted_data.append((current_sender, current_message.strip()))
+            current_sender = sender
+            current_message = message
+
+    formatted_data.append((current_sender, current_message.strip()))
+    return formatted_data
+
+
+process_data = preprocessed_personal_talk_file(cfg.RAW_FILE_PATH)
+save_preprocessed_data(cfg.PROCESSED_FILE_PATH, process_data)
+formatted_data = convert_preprocessed_data(cfg.PROCESSED_FILE_PATH)
+save_preprocessed_data(cfg.PROCESSED_FILE_PATH, formatted_data)
+
+for sender, message in formatted_data:
+    print(f"[{sender}] {message}")
