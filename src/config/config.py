a212382a50193eb966a5d9d4c4715a2fc0ef24e2
myungbin <kki9621@naver.com>
2023-05-31 17:52:47 +0900

    Refactor 데이터 비공개로 전환

---
+++ src/config/config.py
@@ -0,0 +1,8 @@
+class Config:
+    RAW_FILE_PATH = r'C:\MB_Project\project\Myungbin-bot\data\raw\personal talk(kjh).txt'
+    PROCESSED_FILE_PATH = r'C:\MB_Project\project\Myungbin-bot\data\processed\personaltalk.txt'
+    SENDER = '...'
+    RECEIVER = '...'
+
+
+cfg = Config()
