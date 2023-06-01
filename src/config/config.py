b6d7ea9d3a2f005b1c2cfd455e8d40574a19f7fa
myungbin <kki9621@naver.com>
2023-06-01 18:09:52 +0900

    Chore

--- src/config/config.py
+++ src/config/config.py
@@ -1,10 +1,10 @@
 class Config:
     RAW_FILE_PATH = r'C:\MB_Project\project\Myungbin-bot\data\raw\group_talk_nb.txt'
     PROCESSED_FILE_PATH = r'C:\MB_Project\project\Myungbin-bot\data\processed\group_talk_nb.txt'
-    SENDER = ''
-    UNIV_SENDERS = ['17김주형', '17신현재', '17김경민']
-    NB_SENDERS = ['김민규', '김은혁', '백지훈', '이희균']
-    RECEIVER = '곽명빈'
+    SENDER = ...
+    UNIV_SENDERS = ...
+    NB_SENDERS = ...
+    RECEIVER = ...
 
 
 cfg = Config()
